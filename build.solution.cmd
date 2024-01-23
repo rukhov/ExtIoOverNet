3df1991162bf4cbc5bd640682f4e0e2d8449e765
Roman Ukhov <ukhov.roman@gmail.com>
2024-01-23 14:22:09 +0200

    play with actions #1

--- build.solution.cmd
+++ build.solution.cmd
@@ -1,7 +1,8 @@
 set CurPath=%cd%
 
+echo "CurPath: %CurPath%"
 ::cd extio2tcp
 ::cmake -DCMAKE_SYSTEM_VERSION=10.0.19041.0 -S ./ -B ./out -G "Visual Studio 17 2022" -A Win32 --debug-find
-cmake -DCMAKE_SYSTEM_VERSION=10.0.19041.0 -DProtobuf_INSTALL_DIR=%Protobuf_INSTALL_DIR% -S ./ -B ./out -G "Visual Studio 17 2022" -A Win32
+cmake -S ./ -B ./out -G "Visual Studio 17 2022" -A Win32
 
 cd %CurPath%
