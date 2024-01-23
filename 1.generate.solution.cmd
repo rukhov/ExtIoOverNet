5ecbd585deef85adaab78706530594d6da1faaa2
Roman Ukhov <ukhov.roman@gmail.com>
2024-01-23 15:54:22 +0200

    #13

--- 1.generate.solution.cmd
+++ 1.generate.solution.cmd
@@ -5,4 +5,6 @@ echo "CurPath: %CurPath%"
 ::cmake -DCMAKE_SYSTEM_VERSION=10.0.19041.0 -S ./ -B ./out -G "Visual Studio 17 2022" -A Win32 --debug-find
 cmake -S ./ -B ./build -G "Visual Studio 17 2022" -A Win32
 
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 cd %CurPath%
--- 2.build.solution.cmd
+++ 2.build.solution.cmd
@@ -1,8 +1,11 @@
 set CurPath=%cd%
 
-echo "CurPath: %CurPath%"
-
 MSBuild.exe .\build\extio2tcp.sln -t:Build -p:Configuration=Debug
+
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 MSBuild.exe .\build\extio2tcp.sln -t:Build -p:Configuration=RelWithDebInfo
+
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 cd %CurPath%
